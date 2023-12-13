88b2a583e0e001785642fac2132167b054811a74
Cosmin <tifcoosminmoise@gmail.com>
2023-12-13 23:22:56 +0100

    modificado

--- calculadora.sh
+++ calculadora.sh
@@ -3,4 +3,7 @@ read -p "quieres sumar 2 numeros s/n: " respuesta
 if [ $respuesta == s ]; then
 	suma=$(($1+$2))
 	echo "el resultado de la suma es $suma"
+elif [ $respuesta == n ]; then
+	resta=$(($1-$2))
+	echo "el resultado de la resta es $resta"
 fi
